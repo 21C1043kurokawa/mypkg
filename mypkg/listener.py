2c62ffc528afffe8a433b85c6f8624a19e97b585
Daiki <s21C1043OE@s.chibakoudai.jp>
2022-12-02 14:40:36 +0900

    lesson8に戻した

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,16 +1,16 @@
  #!/usr/bin/python3
  # SPDX-FileCopyrightText: 2022 Daiki Kurokawa　　　　
- # SPDX-License-Identifier: BSD-3-Clause
+ # SPDX-License-Identifier: BSD-3-Claus
+
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from std_msgs.msg import Int16
 
 def cb(msg):
-    global node
-    node.get_logger().info("Listen: %s" % msg) #変更
- 
+6   global node
+    node.get_logger().info("Listen: %d" % msg.data)
+
 rclpy.init()
 node = Node("listener")
-pub = node.create_subscription(Person, "person", cb, 10) #変更
-
+pub = node.create_subscription(Int16, "countup", cb, 10)
 rclpy.spin(node)
