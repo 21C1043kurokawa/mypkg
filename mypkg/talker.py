2019977fda23c9e664cff7bba143a7419a3aac44
Daiki <s21C1043OE@s.chibakoudai.jp>
2022-12-08 12:39:13 +0900

    lesson11

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -9,16 +9,19 @@ class Talker():
     def __init__(self,node):
         self.pub = node.create_publisher(Int16,"countup",10)
         self.n = 0
-        node.create_timer(0.5, cb)
+        node.create_timer(0.5, self.cb)
 
     def cb(self):              #関数内のnやpubをtalkerのものに変更
         msg = Int16()
         msg.data = self.n
-        talker.pub.publish(msg)
-        talker.n += 1
+        self.pub.publish(msg)
+        self.n += 1
+def main():
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)      #オブジェクトを作成（__init__が実行される。）
+    rclpy.spin(node)
 
-rclpy.init()
-node = Node("talker")
-talker = Talker(node)      #オブジェクトを作成（__init__が実行される。）
-rclpy.spin(node)
+if __name__ == '__main__':
+      main()
 
